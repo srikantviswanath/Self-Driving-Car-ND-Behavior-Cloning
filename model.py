d5689fd35627c64a92fbd466a8f5b26c29f5a93f
srikantviswanath <srikantviswanath8@gmail.com>
2018-05-13 11:11:51 -0400

    Updated model.py with nvidia_model() and lenet_model()

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential, Model 
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -37,7 +37,7 @@ def flip_images_steer(images, steer_angles):
 	return aug_images, aug_steer_angles
 
 
-def create_model():
+def lenet_model():
 	model = Sequential()
 	model.add(Cropping2D(cropping=((50, 20), (0,  0)), input_shape=(160, 320, 3)))
 	model.add(Lambda(lambda x: (x / 255.0) - 0.5))
@@ -50,6 +50,32 @@ def create_model():
 	model.add(Dense(84))
 	model.add(Dense(1))
 	return model
+
+
+def nvidia_model():
+    model = Sequential()
+    model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
+    model.add(Lambda(lambda img: (img / 255.0) - 0.5))
+    model.add(Convolution2D(filters=24, kernel_size=5, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(filters=36, kernel_size=5, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(filters=48, kernel_size=5, activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(filters=64, kernel_size=3, strides=1))
+    model.add(Convolution2D(filters=64, kernel_size=3, strides=1))
+    model.add(Flatten())
+    model.add(Dense(1164))
+    model.add(Dropout(0.5))
+    model.add(Dense(100))
+    model.add(Dropout(0.5))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    print(model.summary())
+    return model
 
 
 def compile_and_train_model(
@@ -72,8 +98,8 @@ if __name__ == '__main__':
 	images, steer_angles = extract_driving_log(csv_path)
 	augmented_images, augmented_steer_angles = flip_images_steer(images, steer_angles)
 	X_train, y_train = np.array(augmented_images), np.array(augmented_steer_angles)
-	model = create_model()
-	compile_and_train_model(model, X_train, y_train)
+	model = nvidia_model()
+	compile_and_train_model(model, X_train, y_train, epochs=3)
 
 
 
